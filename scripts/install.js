088321f9fdf009d311b2233315b9fef94c0bcbc2
Noah Varghese <varghese.noah@gmail.com>
2021-08-11 16:22:53 -0400

    got themer working, teesting install script and path to resultant config file

---
+++ scripts/install.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+
+(async () => {
+
+    const relativePath = "../../../../components.config.scss";
+    const scssFile = path.resolve(__dirname, relativePath);
+
+    if (!fs.existsSync(scssFile)) {
+        console.error("SCSS config file not found");
+        return;
+    }
+
+    const contents = fs.readFileSync(scssFile);
+    const newFirstLine = `@import "${relativePath};`;
+
+    const rl = readline.createInterface({ input: contents, crlfDelay: Infinity });
+
+    let counter = 0;
+    let newContents = "";
+
+    for await (const line of rl) {
+        if (counter === 0) {
+            newContents += newFirstLine;
+        } else {
+            newContents += line
+        }
+
+        newContents += '\r\n';
+        counter++;
+    }
+
+
+    // const npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
+
+    // cp.spawn(npmCmd, [])
+})();
