95859dd2f33909dbb453519162de78787f715dae
Noah Varghese <varghese.noah@gmail.com>
2021-08-12 08:18:22 -0400

    copy from github

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+const fetch = require("node-fetch");
+
+const newConfigPath = "../../../../components.config.scss";
 
 (async () => {
     if (!fs.existsSync("./lib")) {
@@ -10,10 +13,13 @@ const readline = require("readline");
         return;
     }
 
-    fs.copyFileSync("../default.config.scss", "../../../../components.config.scss");
+    const res = await fetch("https://raw.githubusercontent.com/noahvarghese/react-components/main/default.config.scss");
+    const text = await res.text();
+    console.log(text);
 
-    const relativePath = "../../../../components.config.scss";
-    const scssFile = path.resolve(__dirname, relativePath);
+    fs.writeFileSync(newConfigPath, text);
+
+    const scssFile = path.resolve(__dirname, newConfigPath);
 
     if (!fs.existsSync(scssFile)) {
         console.error("SCSS config file not found");
