ea9492902a53051fad66b0f6771ba740cc5ad4b4
Noah Varghese <varghese.noah@gmail.com>
2021-08-12 17:19:47 -0400

    forgot to reexport

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { default as Button } from "./stories/Button/";
 export { default as Checkbox } from "./stories/Checkbox";
 export { default as FileInput } from "./stories/FileInput";
+export { default as Input } from "./stories/Input";
 
 // behavioural components (or components that are not visible)
 export { default as ProtectedRoute } from "./components/ProtectedRoute";
