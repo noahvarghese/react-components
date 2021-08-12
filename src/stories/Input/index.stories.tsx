c9e1b137a129c276ad06e04fa0bdc271869eb8bc
Noah Varghese <varghese.noah@gmail.com>
2021-08-12 18:46:32 -0400

    added ini error message to input

--- src/stories/Input/index.stories.tsx
+++ src/stories/Input/index.stories.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import validator from "validator";
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Input from ".";
+// import { useState } from '@storybook/addons';
+
+// export default {
+//     title: "Input",
+//     component: Input,
+// } as ComponentMeta<typeof Input>;
+
+let validatorOptions = {
+    validator: (item: any): { success: true; } | { success: false; errorMessage: string } => {
+        let success = false;
+        let errorMessage: string;
+
+        if ((item as string).trim() === "") {
+            errorMessage = "Field cannot be empty";
+
+            return {
+                success,
+                errorMessage: errorMessage ?? undefined
+            }
+        }
+
+        const isEmail = validator.isEmail(item);
+
+        if (isEmail) {
+            success = true;
+        }
+        else {
+            errorMessage = "Invalid email address";
+        }
+
+        return {
+            success,
+            errorMessage: errorMessage ?? undefined
+        }
+    },
+    runOnComplete: true,
+    runOnInput: false
+}
+
+const EmailInput: React.FC = () => {
+    const [emailState, setEmail] = useState("");
+    const [error, setError] = useState("");
+
+
+    return <Input type="email" required={true} name="email" state={{ setState: setEmail, value: emailState }} validationOptions={validatorOptions} errorState={{ setError: setError, value: error }} placeholder="email" />
+}
 
 export default {
     title: "Input",
-    component: Input,
-} as ComponentMeta<typeof Input>;
+    component: EmailInput
+} as ComponentMeta<typeof EmailInput>
 
-const Template: ComponentStory<typeof Input> = args => <Input {...args} />;
-
+const Template: ComponentStory<typeof EmailInput> = (args) => <EmailInput {...args} />
 export const Email = Template.bind({});
-Email.args = {
-    type: "email",
-    readonly: false,
-    required: true,
-    name: "email",
-    placeholder: "email"
-};
--- src/stories/Input/index.tsx
+++ src/stories/Input/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import "./index.scss";
 
 // Need to add in datalist capabilities
 
 interface InputProps {
     type: string;
-    readonly?: boolean;
     name: string;
     placeholder: string;
     required: boolean;
+    readonly?: boolean;
+    state: {
+        value: any;
+        setState: (val: any) => void;
+    },
+    validationOptions?: {
+        validator: (input: any) => { success: true; } | { success: false; errorMessage: string };
+        runOnComplete: boolean;
+        runOnInput: boolean;
+    },
+    dropdownOptions?: {
+        dataList: { id: number; value: string }[];
+        strict: boolean;
+    },
+    errorState?: {
+        value: string | undefined;
+        setError?: (val?: string | undefined) => void;
+    }
 }
 
 const Input: React.FC<InputProps> = (props) => {
+    const validate = useCallback(
+        (value: any, run: boolean | undefined) => {
+            if (props.validationOptions && props.errorState && props.errorState.setError && run) {
+                const res = props.validationOptions.validator(value);
+                if (!res.success) {
+                    props.errorState.setError(res.errorMessage);
+                }
+                else {
+                    props.errorState.setError("");
+                }
+            }
+            props.state.setState(value);
+        },
+        [props.validationOptions],
+    )
     return (
         <div className="Input">
             <input
@@ -19,12 +51,25 @@ const Input: React.FC<InputProps> = (props) => {
                 readOnly={props.readonly}
                 name={props.name}
                 // placeholder={props.placeholder}
-                required={props.required}
+                required={true}
                 id={props.name}
+                onChange={(e) => {
+                    validate(e.currentTarget.value, props.validationOptions?.runOnInput);
+                    console.log("input")
+                }}
+                onBlur={(e) => {
+                    validate(e.currentTarget.value, props.validationOptions?.runOnComplete);
+                    console.log("blur");
+                }}
+                className={props.errorState && props.errorState.value ? "error" : ""}
+                value={props.state.value}
             />
-            <label htmlFor={props.name}>
-                <span>{(props.required && "*") + props.placeholder}</span>
+            <label htmlFor={props.name} className={props.errorState && props.errorState.value ? "error" : ""}>
+                <span>{(props.required && "* ") + props.placeholder}</span>
             </label>
+            {props.errorState && props.errorState.value && <div className="error-message">
+                <p>{props.errorState.value}</p>
+            </div>}
         </div>
     );
 };
