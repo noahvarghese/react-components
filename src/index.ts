26f55c3effab64fa2f2627758412e98206cd390e
Noah Varghese <varghese.noah@gmail.com>
2021-08-13 14:06:20 -0400

    exported new components

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export { default as Button } from "./stories/Button/";
 export { default as Checkbox } from "./stories/Checkbox";
 export { default as FileInput } from "./stories/FileInput";
 export { default as Input } from "./stories/Input";
+export { default as DropdownArrow } from "./stories/DropdownArrow";
+export { default as Select } from "./stories/Select";
 
 // behavioural components (or components that are not visible)
 export { default as ProtectedRoute } from "./components/ProtectedRoute";
