dfad17c94f513e3ee4573f1a00c2b1442a054a91
Noah Varghese <varghese.noah@gmail.com>
2021-08-12 20:07:10 -0400

    started select component

---
+++ src/stories/Select/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DropdownArrow from "../DropdownArrow";
+import "./index.scss";
+
+const Select = () => {
+    return (
+        <div className="select">
+
+        </div>
+    )
+}
+
+export default Select;
