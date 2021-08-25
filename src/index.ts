4b2d3818fdf33ff80759da95574131569f747f49
Noah Varghese <varghese.noah@gmail.com>
2021-08-25 14:02:43 -0400

    added in exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+export { default as AppRouter } from "./components/AppRouter";
 export { default as Button } from "./components/Button";
 export { default as Checkbox } from "./components/Checkbox";
 export { default as DropdownArrow } from "./components/DropdownArrow";
 export { default as FileInput } from "./components/FileInput";
+export { default as Footer } from "./components/Footer";
+export { default as Form } from "./components/Form";
 export { default as HamburgerToggle } from "./components/HamburgerToggle";
 export { default as Input } from "./components/Input";
+export { default as Nav } from "./components/Nav";
 export { default as ProtectedRoute } from "./components/ProtectedRoute";
 export { default as Radio } from "./components/Radio";
 export { default as RadioFieldset } from "./components/RadioFieldset";
 export { default as Select } from "./components/Select";
+
+declare module "*.png";
+declare module "*.jpg";
